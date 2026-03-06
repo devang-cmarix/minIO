4e35ef6369903174f38e7080298229e7b745ac99
Devang <devang.cmarix@gmail.com>
2026-03-06 18:09:02 +0530

    config update

--- connector-service/app/config.py
+++ connector-service/app/config.py
@@ -1,7 +1,12 @@
 import os
 
+#GOOGLE_CREDENTIALS = os.getenv("GOOGLE_CREDENTIALS", "credentials.json")
+
 class Settings:
-    GOOGLE_CREDENTIALS = os.getenv("GOOGLE_CREDENTIALS", "credentials.json")
+    GOOGLE_CREDENTIALS = os.getenv(
+    "GOOGLE_APPLICATION_CREDENTIALS",
+    "/app/credentials.json"
+)
     RABBITMQ_URL = os.getenv("RABBITMQ_URL", "amqp://guest:guest@rabbitmq:5672/")
     QUEUE_NAME = os.getenv("QUEUE_NAME", "file.upload")
     POLL_INTERVAL = int(os.getenv("POLL_INTERVAL", "30"))
